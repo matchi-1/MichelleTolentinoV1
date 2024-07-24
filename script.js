0a69d69d1d33d7e17209e870f64a1e2eacd27fbb
matchi-1 <misheruvynx@gmail.com>
2024-07-24 19:22:30 +0800

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
+// move through sections navi
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll("section");
+  const navLi = document.querySelectorAll(".glass-cont-navi ul li");
+
+  const options = {
+    threshold: 0.7 
+  };
+
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        navLi.forEach(li => {
+          li.classList.remove("active");
+          if (li.querySelector("a").getAttribute("href").substring(1) === entry.target.id) {
+            li.classList.add("active");
+          }
+        });
+      }
+    });
+  }, options);
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+});
+
+// Close the menu if viewport is resized to smallest size
+function handleResize() {
+  const overlayMenu = document.querySelector(".overlay-menu");
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  if (window.innerWidth >= 1100) {
+    overlayMenu.classList.remove("open");
+    hamburgerIcon.classList.remove("open");
+  }
+}
+
+// Add event listener for resize
+window.addEventListener("resize", handleResize);
+
+
+
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
+  const overlayMenu = document.querySelector(".overlay-menu");
   const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
+  overlayMenu.classList.toggle("open");
   icon.classList.toggle("open");
 }
